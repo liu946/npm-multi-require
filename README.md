d6d6c93dc66b6d094240bfd59c4e006807a1f548
liu946 <liuyang570926881@gmail.com>
2016-09-28 18:02:08 +0800

    now commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# npm-multi-require
+
+## Install
+
+```console
+$ npm install --save multi-require
+```
+
+
+## Usage
+
+require other modules in index.js when the file tree is like
+
+```
+.
+├── index.js
+├── testModule1.js
+└── testModule2.js
+
+```
+
+your js code in index.js
+
+```js
+const models = require('multi-require').load(__dirname, {ignore: 'index.js'}); // 1st argument is absolute path
+
+/*
+You will get
+models = {
+    'testModule1': require('testModule1'),
+    'testModule2': require('testModule2'),
+}
+
+*/
+```
+
+```js
+const models = require('multi-require').loadArray(__dirname, {ignore: 'index.js'});
+
+/*
+You will get
+models = [require('testModule1'), require('testModule2')];
+
+*/
+```
+
+
+## License
+
+MIT © [Sindre Sorhus](https://sindresorhus.com)
--- index.js
+++ index.js
@@ -2,3 +2,54 @@
  * Created by liu946 on 16/9/28.
  * Email mkliuyag@163.com
  */
+"use strict";
+
+
+const fs = require('fs');
+const path = require('path');
+
+
+function match(pattern, fileName) {
+  if (pattern === undefined) return false;
+  if (pattern instanceof Array) return pattern.indexOf(fileName);
+  if (pattern instanceof RegExp) return pattern.test(fileName);
+  return pattern === fileName;
+}
+
+/**
+ *
+ * @param dirname String autoLoadDir
+ * @param option Object options
+ * @param option.match 匹配规则
+ * @param option.ignore 忽略规则
+ */
+
+function load (dirname, option) {
+  option = option ? option : {};
+  const modules = {};
+  const files = fs.readdirSync(dirname);
+  files.forEach((file) => {
+    if (/.*\.js$/.test(file)) {
+    if (option.match && !match(option.match, file)) return;
+    if (option.ignore && match(option.ignore, file)) return;
+    modules[path.basename(file, '.js')] = require(path.join(dirname, file));
+  }
+});
+
+  return modules;
+};
+
+function loadDict() {
+  return load.apply(this, arguments);
+}
+
+function loadArray() {
+  const modules = load.apply(this, arguments);
+  return Object.keys(modules).map((k) => modules[k]);
+}
+
+module.exports = {
+  load,
+  loadDict,
+  loadArray,
+};
